f38ce132616832308ca0d83385f9ed012e63f61d
UGUR IZZET ONAL <izzetonal46@gmail.com>
2024-04-15 18:48:01 +0200

    done

---
+++ lab/lab5/task1/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.send('In progress...');
+});
+
+app.use((req, res, next) => {
+    res.status(404).send("Sorry, can't find that!")
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
