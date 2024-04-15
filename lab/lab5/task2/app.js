87cf487f825f9c9c2e2f9c8cfffb0e4adeeaeb58
UGUR IZZET ONAL <izzetonal46@gmail.com>
2024-04-15 19:52:58 +0200

    book added

--- lab/lab5/task2/app.js
+++ lab/lab5/task2/app.js
@@ -1,3 +1,7 @@
+const homeRouter = require('./routes/home');
+const booksRouter = require('./routes/books');
+const authorsRouter = require('./routes/authors');
+
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -7,6 +11,10 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     res.send('In progress...');
 });
+
+app.use('/', homeRouter);
+app.use('/', booksRouter);
+app.use('/', authorsRouter);
 
 app.use((req, res, next) => {
     res.status(404).send("Sorry, can't find that!")
